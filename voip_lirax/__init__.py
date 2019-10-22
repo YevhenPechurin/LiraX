2121f1a5f5660f766c4bdc433b7beea085acc047
Yevhen <eugenpechurin@gmail.com>
2019-10-22 09:51:26 +0300

    [IMP] Update

--- voip_lirax/__init__.py
+++ voip_lirax/__init__.py
@@ -1,1 +1,9 @@
 # -*- coding: utf-8 -*-
+
+from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def uninstall_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env['mail.activity.type'].search([('category', '=', 'phonecall')]).write({'category': 'default'})
--- voip_lirax/__manifest__.py
+++ voip_lirax/__manifest__.py
@@ -2,18 +2,30 @@
 
 {
     'name': "VOIP LiraX",
+
+    'summary': """
+        Make calls using a VOIP LiraX system""",
+
     'description': """
-Module with the required configuration to connect to LiraX.
+        Allow to make call from next activities or with click-to-dial.
     """,
+
     'category': 'Sales',
-    'author': "Yevhen Pechurin",
-    'website': 'https://lirax.ua',
     'version': '1.0',
-    'depends': ['voip'],
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'web', 'phone_validation', 'mail'],
+
+    # always loaded
     'data': [
-        'views/voip_lirax_templates.xml',
+        'security/ir.model.access.csv',
+        'views/assets.xml',
         'views/res_config_settings_views.xml',
+        'views/res_users_views.xml',
+        'views/voip_phonecall_views.xml',
     ],
-    'application': False,
-    'license': 'OPL-1',
+    'qweb': ['static/src/xml/*.xml'],
+    'application': True,
+    'license': 'LGPL-3',
+    'uninstall_hook': "uninstall_hook",
 }
